337b1cdc85abbff50e69029b5bbdfe826f7692e8
jason0539 <jason0539@163.com>
2018-06-02 15:58:51 +0800

    规避异常

--- file/file_read.py
+++ file/file_read.py
@@ -3,12 +3,11 @@ import os
 data = open('sketch.txt')
 
 for each_line in data:
-    try:
+    #if (each_line.find(':') > 0):
+    if not each_line.find(':') == -1:
         (role, role_spoken) = each_line.split(':',1)
         print(role,end='')
         print(' said: ',end='')
         print(role_spoken,end='')
-    except:
-        pass
     
 data.close()
