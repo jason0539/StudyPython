bccaa0f8a98201660c9beabc8ab36e11de6991e7
jason0539 <jason0539@163.com>
2018-06-02 16:03:23 +0800

    增加文件是否存在的检查

--- file/file_read.py
+++ file/file_read.py
@@ -1,13 +1,14 @@
 import os
 
-data = open('sketch.txt')
-
-for each_line in data:
-    #if (each_line.find(':') > 0):
-    if not each_line.find(':') == -1:
-        (role, role_spoken) = each_line.split(':',1)
-        print(role,end='')
-        print(' said: ',end='')
-        print(role_spoken,end='')
-    
-data.close()
+if os.path.exists('sketch.txt'):
+    data = open('sketch.txt')
+    for each_line in data:
+        #if (each_line.find(':') > 0):
+        if not each_line.find(':') == -1:
+            (role, role_spoken) = each_line.split(':',1)
+            print(role,end='')
+            print(' said: ',end='')
+            print(role_spoken,end='')        
+    data.close()
+else:
+    print('The data file is missing!')
--- file/file_read_catch_exception.py
+++ file/file_read_catch_exception.py
@@ -1,14 +1,15 @@
 import os
 
-data = open('sketch.txt')
-
-for each_line in data:
-    try:
-        (role, role_spoken) = each_line.split(':',1)
-        print(role,end='')
-        print(' said: ',end='')
-        print(role_spoken,end='')
-    except:
-        pass
-    
-data.close()
+if os.path.exists('sketch.txt'):
+    data = open('sketch.txt')
+    for each_line in data:
+        try:
+            (role, role_spoken) = each_line.split(':',1)
+            print(role,end='')
+            print(' said: ',end='')
+            print(role_spoken,end='')        
+        except:
+            pass    
+    data.close()
+else:
+    print('The data file is missing!')
