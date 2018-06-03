7a70088e796d88b49f72083e5598826508481c04
jason0539 <jason0539@163.com>
2018-06-03 08:00:08 +0800

    使用with操作文件

--- file/file_save.py
+++ file/file_save.py
@@ -18,14 +18,32 @@ try:
 except IOError:
     print('The data file is missing!')
 
-try:
-    man_file = open('man_data.txt','w')
-    other_file = open('other_data.txt','w')
+'''文件打开写入的常规方式'''
+# try:
+#     man_file = open('man_data.txt','w')
+#     other_file = open('other_data.txt','w')
 
-    print(man,file=man_file)
-    print(other,file=other_file)
-except IOError:
-    print('File error.')
-finally:
-    man_file.close()
-    other_file.close()
+#     print(man,file=man_file)
+#     print(other,file=other_file)
+# except IOError:
+#     print('File error.')
+# finally:
+#     man_file.close()
+#     other_file.close()
+
+''' 使用with处理文件打开与写入'''
+try:
+    with open('man_data.txt','w') as man_file:
+        print(man,file = man_file)
+    with open('other_data.txt','w') as other_file:
+        print(other,file = other_file)
+except IOError as err:
+    print('File error:' + str(err))
+
+'''简化成一行'''
+# try:
+#     with open('man_data.txt','w') as man_file,with open('other_data.txt','w') as other_file:
+#         print(man,file = man_file)
+#         print(other,file = other_file)
+# except IOError as err:
+#     print('File error:' + str(err))
