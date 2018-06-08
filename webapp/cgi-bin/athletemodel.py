a505318b45001de66ded7b03760478cdf6903f2a
jason0539 <jason0539@163.com>
2018-06-09 06:33:32 +0800

    从数据库加载数据的model

--- webapp/cgi-bin/athletemodel.py
+++ webapp/cgi-bin/athletemodel.py
@@ -4,13 +4,37 @@ import sqlite3
 
 from athletelist import AthleteList
 
-db_name = '../../sqlite/coachdata.sqlite'
+db_name = 'coachdata.sqlite'
 
 def get_names_from_store():
     connection = sqlite3.connect(db_name)
     cursor = connection.cursor()
     results = cursor.execute("""SELECT name FROM athletes""")
     response = [row[0] for row in results.fetchall()]
+    connection.close()
+    return(response)
+
+def get_namesID_from_store():
+    connection = sqlite3.connect(db_name)
+    cursor = connection.cursor()
+    results = cursor.execute("""SELECT name,id FROM athletes""")
+    response = results.fetchall()
+    connection.close()
+    return(response)
+
+def get_athlete_from_id(athlete_id):
+    connection = sqlite3.connect(db_name)
+    cursor = connection.cursor()
+    results =  cursor.execute("""select name,dob from athletes where id=?""",(athlete_id))
+    (name,dob) = results.fetchone()
+    results = cursor.execute("""select value from timing_data where athlete_id=?""",(athlete_id))
+    data = [row[0] for row in results.fetchall()]
+    response = {
+        'Name':name,
+        'Dob':dob,
+        'data':data,
+        'top3':data[0:3]
+    }
     connection.close()
     return(response)
 
